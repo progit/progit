15c333fc3e4554911f78d81771e9d5cbcdb374ef
Jean-Noel Avila <jn.avila@free.fr>
2011-03-04 23:30:16 +0100

    summary.rb: corrected progress computing to better reflect actual state

--- summary.rb
+++ summary.rb
@@ -5,10 +5,10 @@ command = ARGV[0]
 exclude = ['figures', 'figures-dia', 'figures-source', 'couchapp', 'latex', 'pdf', 'epub', 'en', 'ebooks']
 
 data = []
-original_lines=`grep -r '^[^[:space:]]' en/[0]* | wc -l`.to_i
+original_lines=`grep -r -h '^[^[:space:]#]' en/[0]* | grep -v '^Insert'| wc -l`.to_i
 Dir.glob("*").each do |dir|
   if !File.file?(dir) && !exclude.include?(dir)
-    lines = `git diff-tree -r -p master:en master:#{dir} | grep '^-[^[:space:]]' | wc -l`.strip.to_i
+    lines = `git diff-tree -r -p --diff-filter=M master:en master:#{dir} | grep '^-[^[:space:]#-]' | wc -l`.strip.to_i
     last_commit = `git log -1 --no-merges --format="%ar" #{dir}`.chomp
     authors = ""
     if command == 'authors'
@@ -20,7 +20,7 @@ end
 
 d = data.sort { |a, b| b[1] <=> a[1] }
 d.each do |dir, lines, authors, last|
-  puts "#{dir.ljust(10)} - #{lines*100/original_lines}% (#{last})"
+  puts "#{dir.ljust(10)} - #{(lines*100)/original_lines}% (#{last})"
   if command == 'authors'
     puts "Authors: #{authors.split("\n").size}"
     puts authors 
